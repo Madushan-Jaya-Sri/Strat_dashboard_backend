5ee006e881d1ed8607fe1a715e9d535941284261
Madushan-Jaya-Sri <111506676+Madushan-Jaya-Sri@users.noreply.github.com>
2025-09-09 21:45:48 +0530

    updated scopes

--- auth/auth_manager.py
+++ auth/auth_manager.py
@@ -61,13 +61,13 @@ class AuthManager:
         
         self.FACEBOOK_SCOPES = [
             'email',
-            'ads_read',
-            'ads_management',
-            'business_management',
+            # 'ads_read',
+            # 'ads_management',
+            # 'business_management',
             'public_profile',
-            'pages_show_list',           # Required to see pages
-            'pages_read_engagement',     # Required to read page data
-            'instagram_basic'            # For Instagram Business accounts
+            # 'pages_show_list',           # Required to see pages
+            # 'pages_read_engagement',     # Required to read page data
+            # 'instagram_basic'            # For Instagram Business accounts
         ]
         
         # In-memory session storage (use Redis in production)
